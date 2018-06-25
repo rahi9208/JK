539c899ce9ad7567f111859062c08fe8b49d1a68
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-25 10:49:31 +0530

    cmm

--- jk/lambda.js
+++ jk/lambda.js
@@ -1,8 +1,6 @@
 let AWS = require('aws-sdk');
-let firebase = require('firebase');
-exports.handler = function (event, context, callback) {
-	
 
-
-	callback(null, 'Successfully executed');
+exports.handler = function(event, context, callback) {
+    
+    callback(null,'Successfully executed');
 }
