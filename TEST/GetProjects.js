267d3046c5563ef2e90da7f92ae07317440aa511
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-07-02 17:50:14 +0530

    added project query

--- TEST/GetProjects.js
+++ TEST/GetProjects.js
@@ -1,6 +1,27 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function(event, context, callback) {
-    
-    callback(null,'Successfully executed');
+exports.handler = function (event, context, callback) {
+    let response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": "..."
+    };
+
+    let username = event.queryStringParameters.username;
+
+    ddb.query({
+        TableName: 'SigmaProjects',
+        ExpressionAttributeValues: { ':username': username },
+        KeyConditionExpression: 'id = :username',
+    }, function (err, data) {
+        if (!err) {
+            response.body = JSON.stringify(data.Items);
+        }
+        callback(err, response);
+    });
+
+
 }
