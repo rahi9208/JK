28a2138e674f1c0e6b5239d3821b84adc786bed7
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-07-03 10:48:07 +0530

    changing dynamodb structure

---
+++ TEST/SaveProject.js
@@ -0,0 +1,21 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+let uuid = require('uuid');
+
+exports.handler = function (event, context, callback) {
+
+    let projectId = event.projectId || uuid.v4();
+    
+    ddb.put({
+        TableName: 'SigmaUserProjects',
+        Item: {
+            'name': event.name,
+            'username': event.username,
+            'projectId': event.projectId,
+            'description': event.description,
+            'functions': event.functions
+        }
+    }, function (err, data) {
+        callback(err, 'Successfully executed');
+    });
+}
