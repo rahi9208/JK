75a3f37b5db18d6777d40432963bc5d50110ecf1
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-07-04 17:53:32 +0530

    enabling cors for delete

--- TEST/RemoveProject.js
+++ TEST/RemoveProject.js
@@ -2,18 +2,27 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
+
+    let response = {
+        "isBase64Encoded": 1,
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": "..."
+    }
+
     let username = event.queryStringParameters.username;
     let projectId = event.queryStringParameters.projectId;
     ddb.delete({
         TableName: 'SigmaUserProjects',
         Key: { 'username': username, 'projectId': projectId }
     }, function (err, data) {
-        if (err) {
-            //handle error
-        } else {
-            //your logic goes here
+        if (!err) {
+            response.body = "Successfully deleted " + projectId;
+        }else{
+            response.statusCode = 500;
         }
+        callback(err,response);
     });
-
-    callback(null, 'Successfully executed');
 }
