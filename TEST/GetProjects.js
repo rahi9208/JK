f8866baf4c727810ddbbf3e39e339bb949e06c71
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-07-05 16:12:38 +0530

    adding single project fetch

--- TEST/GetProjects.js
+++ TEST/GetProjects.js
@@ -4,8 +4,8 @@ let Auth = require('./Auth');
 
 exports.handler = function (event, context, callback) {
 
-    Auth.wrap(event,  callback, (username) => {
-        
+    Auth.wrap(event, callback, (username) => {
+
         let response = {
             "statusCode": 200,
             "headers": {
@@ -14,20 +14,39 @@ exports.handler = function (event, context, callback) {
             "body": "..."
         };
 
-        console.log("Searching for", username);
+        let projectId = event.queryStringParameters.projectId;
 
-        ddb.query({
-            TableName: 'SigmaUserProjects',
-            ExpressionAttributeValues: { ':username': username },
-            KeyConditionExpression: 'username = :username',
-        }, function (err, data) {
-            if (!err) {
-                console.log("Successfully queried", data.Items.length, "for", username);
-                response.body = JSON.stringify(data.Items);
-            } else {
-                console.error("Error occurred when searching for projects of", username, err);
-            }
-            callback(err, response);
-        });
+        if (!projectId) {
+            console.log("Searching all projects for", username);
+
+            ddb.query({
+                TableName: 'SigmaUserProjects',
+                ExpressionAttributeValues: { ':username': username },
+                KeyConditionExpression: 'username = :username',
+            }, function (err, data) {
+                if (!err) {
+                    console.log("Successfully queried", data.Items.length, "for", username);
+                    response.body = JSON.stringify(data.Items);
+                } else {
+                    response.statusCode = 500;
+                    console.error("Error occurred when searching for projects of", username, err);
+                }
+                callback(err, response);
+            });
+        } else {
+            ddb.get({
+                TableName: 'SigmaUserProjects',
+                Key: { 'username': username, 'projectId': projectId }
+            }, function (err, data) {
+                if (!err) {
+                    console.log("Successfully queried project %s of %s", projectId, username);
+                    response.body = JSON.stringify(data.Item);
+                } else {
+                    console.error("Error in fecthing %s of %s", projectId, username);
+                    esponse.statusCode = 500;
+                }
+                callback(err, response);
+            });
+        }
     });
 }
